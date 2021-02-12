3c5ec626b6ec6b11cb1aa4a08e00bd5542a98dd1
AlCor <licone7@yandex.ru>
2021-02-12 10:47:03 +0400

    start

---
+++ src/OutputConsole.java
@@ -0,0 +1,13 @@
+public class OutputConsole {
+
+
+    public void startMessage(){
+        System.out.print("Введитете шаг сдвига ");
+    }
+
+    public void transcript(String textTranscript){
+        System.out.println(textTranscript);
+    }
+
+
+}
